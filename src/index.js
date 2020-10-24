6b5480b0d8d6c9d9750d68ebd2c71856bfb417c1
Valeryia Klopava <bsu.valeria.kl@gmail.com>
2020-10-24 19:19:26 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!Array.isArray(matrix) || matrix == []) return [];
+  else {  
+    let i = 1;
+    while (i < matrix.length) {
+      matrix[i] = matrix[i].reverse()
+      i += 2;
+    }
+  }
+    return matrix.flat(Infinity);
+};
